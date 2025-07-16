9214192071355ceb18eea3a90f2a032f210e8838
Timothy <152324947+tails1154@users.noreply.github.com>
2025-07-16 18:28:38 -0500

    Update serial_device_network.cpp

--- src/core/serial/serial_device_network.cpp
+++ src/core/serial/serial_device_network.cpp
@@ -1,23 +1,52 @@
-#pragma once
-#include "serial_device.hpp"
-#include <string>
+#include "serial_device_network.hpp"
+#include <iostream>
+
+SerialDeviceNetwork::SerialDeviceNetwork(const std::string& ip, int port, bool is_server)
+    : is_server(is_server), last_bit(1)
+{
 #ifdef _WIN32
-#include <winsock2.h>
-#else
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,2), &wsaData);
 #endif
 
-class SerialDeviceNetwork : public SerialDevice {
-public:
-    SerialDeviceNetwork(const std::string& ip, int port, bool is_server);
-    ~SerialDeviceNetwork();
-    bool send() override;
-    void receive(bool bit) override;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) throw std::runtime_error("socket failed");
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
 
-private:
-    int sockfd;
-    bool is_server;
-    uint8_t last_bit;
-};
+    if (is_server) {
+        bind(sockfd, (sockaddr*)&addr, sizeof(addr));
+        listen(sockfd, 1);
+        int clientfd = accept(sockfd, nullptr, nullptr);
+        if (clientfd < 0) throw std::runtime_error("accept failed");
+        // Use clientfd for comms, close sockfd later
+        sockfd = clientfd;
+    } else {
+        if (connect(sockfd, (sockaddr*)&addr, sizeof(addr)) < 0)
+            throw std::runtime_error("connect failed");
+    }
+}
+
+SerialDeviceNetwork::~SerialDeviceNetwork() {
+#ifdef _WIN32
+    closesocket(sockfd);
+    WSACleanup();
+#else
+    close(sockfd);
+#endif
+}
+
+bool SerialDeviceNetwork::send() {
+    // Send last_bit to peer
+    send(sockfd, (char*)&last_bit, 1, 0);
+    // Receive bit from peer
+    uint8_t peer_bit = 1;
+    recv(sockfd, (char*)&peer_bit, 1, MSG_WAITALL);
+    return peer_bit;
+}
+
+void SerialDeviceNetwork::receive(bool bit) {
+    last_bit = bit;
+}
